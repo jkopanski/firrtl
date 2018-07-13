899424169e160903689d1de88528439ecb9c918f
Jakub Kopański <jkopanski@protonmail.com>
2018-07-13 12:43:21 +0200

    typechecking whole circuit

--- src/Firrtl/Lo/TypeCheck/Circuit.hs
+++ src/Firrtl/Lo/TypeCheck/Circuit.hs
@@ -3,6 +3,8 @@
       , TypeInType #-}
 module Firrtl.Lo.TypeCheck.Circuit where
 
+import Control.Monad.Except (throwError)
+import qualified Data.List.NonEmpty as NE
 import Data.Singletons.Prelude hiding (Error)
 import Data.Nat
 
@@ -10,19 +12,30 @@ import qualified Firrtl.Lo.Syntax.Safe as Safe
 import           Firrtl.Lo.Syntax.Circuit
 import           Firrtl.Lo.Syntax.Stmt
 import           Firrtl.Lo.TypeCheck.Monad
+import           Firrtl.Lo.TypeCheck.Stmt
 import           Firrtl.Lo.TypeCheck.Ty
 
 instance Typed Circuit where
   type TypeSafe Circuit = Safe.Circuit
 
   typeSafe :: Circuit -> Check Safe.Circuit
-  typeSafe = undefined
+  typeSafe (Circuit ident modules) = do
+    let mainModules = NE.filter ((ident ==) . moduleName) modules
+        numMains = length mainModules
+
+    if numMains /= 1
+       then throwError $ NoTopModule ident
+       else Safe.Circuit ident <$> traverse typeSafe modules
   
 instance Typed Module where
   type TypeSafe Module = Safe.Module
 
   typeSafe :: Module -> Check Safe.Module
-  typeSafe = undefined
+  typeSafe (ExtModule ident ports) =
+    Safe.ExtModule ident <$> (traverse typeSafe ports)
+
+  typeSafe (Module ident ports body) = do
+    Safe.Module ident <$> traverse typeSafe ports <*> typeSafe body
 
 instance Typed Port where
   type TypeSafe Port = Safe.SomePort
