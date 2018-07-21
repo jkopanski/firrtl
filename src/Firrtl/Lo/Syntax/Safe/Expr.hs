97aee4b934ff96a1a98032091dc3368859a15d6b
Jakub Kopański <jkopanski@protonmail.com>
2018-07-21 18:11:53 +0200

    higher order functor thx to kinder-functor

--- src/Firrtl/Lo/Syntax/Safe/Expr.hs
+++ src/Firrtl/Lo/Syntax/Safe/Expr.hs
@@ -46,12 +46,11 @@ module Firrtl.Lo.Syntax.Safe.Expr
   , mkMux
 
   -- Traversals
+  , Functor
   ) where
 
-import Prelude hiding ((.), id, Functor (..))
-import qualified Prelude
-import Control.Category
-import Control.Categorical.Functor
+import Prelude hiding (Functor (fmap), id, (.))
+import Control.Category (Category (id, (.)))
 
 import Data.Kind (type (*))
 import Data.Singletons
@@ -172,6 +171,62 @@ type Expr = TFix ExprF
 
 -- instance HFunctor ExprF where
 --   hfmap f (UInt s n) = UInt s n
+
+
+-- Kinder functor formulation
+-- ripped from https://github.com/rampion/kinder-functor
+type family Cat k :: k -> k -> *
+
+class (Category (Cat j), Category (Cat k)) => Functor (f :: j -> k) where
+  fmap :: Cat j a b -> Cat k (f a) (f b)
+
+type instance Cat (*) = (->)
+
+infixr 0 $$
+
+newtype NT (cat :: k -> k -> *) (f :: j -> k) (g :: j -> k) =
+  NT { ($$) :: forall x. cat (f x) (g x) }
+
+instance Category cat => Category (NT cat) where
+  id = NT id
+  NT a . NT b = NT (a . b) 
+
+type instance Cat (j -> k) = NT (Cat k)
+
+data CTy (a :: Ty) (b :: Ty) where
+  CId :: CTy a a
+  -- CF  :: CTy a b -> a -> b -> CTy a b
+  CC  :: forall (a :: Ty) (b :: Ty) (c :: Ty). CTy b c -> CTy a b -> CTy a c
+
+type instance Cat Ty = CTy
+
+instance Category CTy where
+  id = CId
+  (.) = CC
+
+instance Functor (CTy a) where
+  fmap = (.)
+
+instance (Category cat, Cat k ~ cat) => Functor (NT cat f) where
+  fmap = (.)
+
+instance Functor NT where
+  fmap h = NT $ NT $ \(NT h') -> NT $ (($$) h) $$ h'
+
+instance Functor ExprF where
+  fmap (NT h) = NT $ \ex -> case ex of
+    UInt s n -> UInt s n
+    SInt s i -> SInt s i
+    Ref  s i -> Ref  s i
+    Valid s cond a -> Valid s (h cond) (h a)
+    Mux s cond a b -> Mux s (h cond) (h a) (h b)
+    -- Valid s cond a -> Valid (( fmap h ) cond) (( fmap h ) a) -- fmap h _  -- (fmap a)
+    -- -- where g :: ExprF a t -> ExprF b t
+         --  UInt (STuple3 SUnsigned SZ SMale) 1
+  
+
+--   fmap CId t = t
+--   fmap CC a = CC . a
 
 -- class TFunctor (h :: (Ty s n g -> *) -> Ty s n g -> *) where
 --   tfmap :: (e :~> f) -> h e :~> h f
